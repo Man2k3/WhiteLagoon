01de80a982ff9e93de54f86de6210e2ed1d5df61
Man2k3 <Anmanproone94@gmail.com>
2024-10-12 20:57:53 +0700

    FinishSS9

--- WhiteLagoon.Infrastructure/Migrations/20241012035033_addTableAmenity.Designer.cs
+++ WhiteLagoon.Infrastructure/Migrations/20241012035033_addTableAmenity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WhiteLagoon.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using WhiteLagoon.Infrastructure.Data;
 namespace WhiteLagoon.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241012035033_addTableAmenity")]
+    partial class addTableAmenity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -186,7 +189,7 @@ namespace WhiteLagoon.Infrastructure.Migrations
                         });
                 });
 
-            modelBuilder.Entity("WhiteLagoon.Domain.Entities.VillaNumber", b =>
+            modelBuilder.Entity("WhiteLagoon.Domain.Entities.amenities", b =>
                 {
                     b.Property<int>("Villa_Number")
                         .HasColumnType("int");
@@ -201,7 +204,7 @@ namespace WhiteLagoon.Infrastructure.Migrations
 
                     b.HasIndex("VillaId");
 
-                    b.ToTable("VillaNumbers");
+                    b.ToTable("amenitiess");
 
                     b.HasData(
                         new
@@ -262,7 +265,7 @@ namespace WhiteLagoon.Infrastructure.Migrations
                     b.Navigation("Villa");
                 });
 
-            modelBuilder.Entity("WhiteLagoon.Domain.Entities.VillaNumber", b =>
+            modelBuilder.Entity("WhiteLagoon.Domain.Entities.amenities", b =>
                 {
                     b.HasOne("WhiteLagoon.Domain.Entities.Villa", "Villa")
                         .WithMany()
---
+++ WhiteLagoon.Web/Controllers/AmenityController.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Web.ViewModels;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class AmenityController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public AmenityController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var Amenitys = _unitOfWork.Amenity.GetAll(filter: null, includeProperties: "Villa");
+            return View(Amenitys);
+        }
+        public IActionResult Create()
+        {
+            AmenityVM AmenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                })
+            };
+
+            return View(AmenityVM);
+        }
+        [HttpPost]
+        public IActionResult Create(AmenityVM obj)
+        {
+            
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Add(obj.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            
+            obj.VillaList = _unitOfWork.Villa.GetAll().Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+            return View(obj);
+        }
+        public IActionResult Update(int AmenityId)
+        {
+            AmenityVM AmenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(x => x.Id == AmenityId)
+            };
+            if (AmenityVM == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(AmenityVM);
+        }
+        [HttpPost]
+        public IActionResult Update(AmenityVM AmenityVM)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Amenity.Update(AmenityVM.Amenity);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been updated successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            AmenityVM.VillaList = _unitOfWork.Villa.GetAll().Select(x => new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            });
+            return View(AmenityVM);
+        }
+        public IActionResult Delete(int AmenityId)
+        {
+            AmenityVM AmenityVM = new()
+            {
+                VillaList = _unitOfWork.Villa.GetAll().Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.Id.ToString()
+                }),
+                Amenity = _unitOfWork.Amenity.Get(x => x.Id == AmenityId)
+            };
+            if (AmenityVM == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(AmenityVM);
+        }
+        [HttpPost]
+        public IActionResult Delete(AmenityVM AmenityVM)
+        {
+            Amenity? objFormDb = _unitOfWork.Amenity.Get(x => x.Id == AmenityVM.Amenity.Id);
+            if (objFormDb is not null)
+            {
+                _unitOfWork.Amenity.Remove(objFormDb);
+                _unitOfWork.Save();
+                TempData["success"] = "The amenity has been deleted successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+            TempData["error"] = "The amenity could not be deleted.";
+            return View();
+        }
+    }
+}
+
